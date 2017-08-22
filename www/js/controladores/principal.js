4f81318cdde5903e22cc53328aaef3e01f44e5ef
isidoro <isidoro@bbbb4ca1-6a7b-484a-b85f-4810571af716>
2017-08-22 11:10:01 +0000

    git-svn-id: svn://repo.sion/appMoviles/AndroidSensor@3769 bbbb4ca1-6a7b-484a-b85f-4810571af716

--- www/js/controladores/principal.js
+++ www/js/controladores/principal.js
@@ -3,8 +3,8 @@
  */
 app.controller("CtrlPrincipal", function($scope, $ionicPopover) {
 	$scope.estado = {
-		mqtt : true,
-		arduinoCon : true,
+		mqtt : false,
+		arduinoCon : false,
 		arduinoIni : false,
 		servicio : false
 	};
@@ -33,5 +33,12 @@ app.controller("CtrlPrincipal", function($scope, $ionicPopover) {
 
 	$scope.iniciarDetener = function() {
 		$scope.estado.servicio = !$scope.estado.servicio;
+		if ($scope.estado.servicio) {
+
+		} else {
+			$scope.estado.mqtt = false;
+			$scope.estado.arduinoCon = false;
+			$scope.estado.arduinoIni = false;
+		}
 	};
 });
