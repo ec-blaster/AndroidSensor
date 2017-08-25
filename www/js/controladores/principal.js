bf55fb2a6a0f3b28b6472b434ea85fd13994160e
isidoro <isidoro@bbbb4ca1-6a7b-484a-b85f-4810571af716>
2017-08-25 11:51:53 +0000

    git-svn-id: svn://repo.sion/appMoviles/AndroidSensor@3792 bbbb4ca1-6a7b-484a-b85f-4810571af716

--- www/js/controladores/principal.js
+++ www/js/controladores/principal.js
@@ -1,160 +1,225 @@
 /**
  * Controlador de la pantalla principal
  */
-app.controller("CtrlPrincipal", function($scope, $rootScope, $ionicPopover,
-		$ionicPopup, $cordovaDevice, MqttClient) {
-	$scope.estado = {
-		mqtt : false,
-		arduinoCon : false,
-		arduinoIni : false,
-		servicio : false
-	};
+app
+		.controller(
+				"CtrlPrincipal",
+				function($scope, $rootScope, $ionicPopover, $ionicPopup,
+						$cordovaDevice, MqttClient) {
+					$scope.estado = {
+						mqtt : false,
+						arduinoCon : false,
+						arduinoIni : false,
+						servicio : false
+					};
 
-	$scope.conectando = false;
-	$scope.conectandoMQTT = false;
+					$scope.strRecibida = '';
 
-	// Definimos un menú popover:
-	$ionicPopover.fromTemplateUrl('vistas/menu.html', {
-		scope : $scope
-	}).then(function(popover) {
-		$scope.menu = popover;
-	});
+					$scope.conectando = false;
+					$scope.conectandoMQTT = false;
 
-	// Abrir el menú
-	$scope.mostrarMenu = function($event) {
-		$scope.menu.show($event);
-	};
+					// Definimos un menú popover:
+					$ionicPopover.fromTemplateUrl('vistas/menu.html', {
+						scope : $scope
+					}).then(function(popover) {
+						$scope.menu = popover;
+					});
 
-	// Cerrar el menú
-	$scope.cerrarMenu = function() {
-		$scope.menu.hide();
-	};
+					// Abrir el menú
+					$scope.mostrarMenu = function($event) {
+						$scope.menu.show($event);
+					};
 
-	// Destruir el menú
-	$scope.$on('$destroy', function() {
-		$scope.menu.remove();
-	});
+					// Cerrar el menú
+					$scope.cerrarMenu = function() {
+						$scope.menu.hide();
+					};
 
-	// Obtenemos un ID único del dispositivo
-	$scope.getIdDispositivo = function() {
-		var id = "";
-		if (window.device)
-			id = $cordovaDevice.getUUID();
-		else
-			id = CryptoJS.MD5(window.navigator.userAgent).toString(
-					CryptoJS.enc.Hex);
+					// Destruir el menú
+					$scope.$on('$destroy', function() {
+						$scope.menu.remove();
+					});
 
-		return 'android_' + id;
-	};
+					// Obtenemos un ID único del dispositivo
+					$scope.getIdDispositivo = function() {
+						var id = "";
+						if (window.device)
+							id = $cordovaDevice.getUUID();
+						else
+							id = CryptoJS.MD5(window.navigator.userAgent)
+									.toString(CryptoJS.enc.Hex);
 
-	// Conectamos con el servidor
-	$scope.conectarMQTT = function(servidor, puerto, usr, pwd) {
+						return 'android_' + id;
+					};
 
-		console.log("Conectando con el broker " + servidor + " con el usuario "
-				+ usr);
-		MqttClient.init(servidor, puerto, $scope.getIdDispositivo());
-		$scope.conectandoMQTT = true;
-		MqttClient.connect({
-			timeout : 5,
-			onSuccess : $scope.MQTTconectado,
-			onFailure : function(err) {
-				console.log("Error de conexión: " + err.errorMessage);
-				$scope.desconectarTodo();
-				$ionicPopup.alert({
-					title : 'Error de conexión MQTT',
-					template : err.errorMessage
+					// Conectamos con el servidor
+					$scope.conectarMQTT = function(servidor, puerto, usr, pwd) {
+
+						console.log("Conectando con el broker " + servidor
+								+ " con el usuario " + usr);
+						MqttClient.init(servidor, puerto, $scope
+								.getIdDispositivo());
+						$scope.conectandoMQTT = true;
+						MqttClient.connect({
+							timeout : 5,
+							onSuccess : $scope.MQTTconectado,
+							onFailure : function(err) {
+								console.log("Error de conexión: "
+										+ err.errorMessage);
+								$scope.desconectarTodo();
+								$ionicPopup.alert({
+									title : 'Error de conexión MQTT',
+									template : err.errorMessage
+								});
+							}
+						});
+					};
+
+					// Hemos conectado con el servidor MQTT
+					$scope.MQTTconectado = function() {
+						console
+								.log("Conectado con "
+										+ $rootScope.mqtt.servidor);
+						$scope.conectandoMQTT = false;
+						$scope.estado.mqtt = true;
+						$scope.$apply();
+
+						/*
+						 * var topico = "sion/pruebas";
+						 * MqttClient.subscribe(topico); message = new
+						 * Paho.MQTT.Message("Hola"); message.destinationName =
+						 * topico; MqttClient.send(message);
+						 */
+
+						$scope.conectarSerie();
+					};
+
+					// Nos conectamos al puerto serie del arduino (USB OTG)
+					$scope.conectarSerie = function() {
+						if (typeof (window.serial) != "undefined") {
+							// Pedimos permiso para conectarnos al puerto serie
+							console.log('Solicitando permiso para USB...');
+							window.serial
+									.requestPermission(
+											function() {
+												// Hemos obtenido permiso
+												console
+														.log('Permiso concedido');
+												var opc = {
+													baudRate : 9600,
+													sleepOnPause : true
+												// Mantenemos el puerto abierto
+												// cuando la app pasa a 2º plano
+												};
+
+												// Nos conectamos
+												console
+														.log('Abriendo puerto serie...');
+												window.serial
+														.open(
+																opc,
+																function() {
+																	console
+																			.log('Puerto abierto');
+																	$scope.estado.arduinoCon = true;
+
+																	// Registramos
+																	// un
+																	// callback
+																	// para la
+																	// recepción
+																	// de datos
+																	// desde
+																	// el
+																	// Arduino:
+																	window.serial
+																			.registerReadCallback(
+																					function(
+																							data) {
+																						var view = new Uint8Array(
+																								data);
+																						if (view.length >= 1) {
+																							for (var i = 0; i < view.length; i++) {
+																								if (view[i] == 10) {
+																									$scope
+																											.comandoRecibido($scope.strRecibida);
+																									$scope.strRecibida = '';
+																								} else {
+																									var temp_str = String
+																											.fromCharCode(view[i]);
+																									var str_esc = escape(temp_str);
+																									$scope.strRecibida += unescape(str_esc);
+																								}
+																							}
+
+																						}
+																					},
+																					function() {
+																						errorSerie("Error al registrar lecturas");
+																					});
+
+																	$scope
+																			.todoConectado();
+																},
+																$scope.errorSerie);
+											}, $scope.errorSerie);
+						} else
+							$scope
+									.errorSerie("Dispositivo serie no disponible");
+
+					};
+
+					$scope.comandoRecibido = function(comando) {
+						console.log('Comando recibido: ' + comando);
+					};
+
+					$scope.errorSerie = function(err) {
+						console.log(err);
+						$scope.desconectarTodo();
+						$ionicPopup.alert({
+							title : 'Error de conexión USB',
+							template : err
+						});
+					};
+
+					$scope.todoConectado = function() {
+						$scope.conectando = false;
+						$scope.estado.servicio = true;
+						$scope.$apply();
+					};
+
+					$scope.desconectarTodo = function() {
+						if ($scope.estado.mqtt || $scope.conectandoMQTT) {
+							console.log('Desconectamos MQTT');
+							if (MqttClient.connected)
+								MqttClient.disconnect();
+						}
+
+						if ($scope.estado.arduinoCon) {
+							console.log('Cerramos puerto serie');
+							window.serial.close(function() {
+								console.log('Puerto cerrado');
+							}, function() {
+							});
+						}
+
+						$scope.estado.mqtt = false;
+						$scope.estado.arduinoCon = false;
+						$scope.estado.arduinoIni = false;
+						$scope.conectando = false;
+						$scope.conectandoMQTT = false;
+						$scope.estado.servicio = false;
+					};
+
+					$scope.iniciarDetener = function() {
+						if ($scope.conectando || $scope.estado.servicio) {
+							$scope.desconectarTodo();
+						} else {
+							$scope.conectando = true;
+							$scope.conectarMQTT($rootScope.mqtt.servidor,
+									$rootScope.mqtt.puerto,
+									$rootScope.mqtt.usuario,
+									$rootScope.mqtt.password);
+						}
+					};
 				});
-			}
-		});
-	};
-
-	// Hemos conectado con el servidor MQTT
-	$scope.MQTTconectado = function() {
-		console.log("Conectado con " + $rootScope.mqtt.servidor);
-		$scope.conectandoMQTT = false;
-		$scope.estado.mqtt = true;
-		$scope.$apply();
-
-		/*
-		 * var topico = "sion/pruebas"; MqttClient.subscribe(topico); message =
-		 * new Paho.MQTT.Message("Hola"); message.destinationName = topico;
-		 * MqttClient.send(message);
-		 */
-
-		$scope.conectarSerie();
-	};
-
-	// Nos conectamos al puerto serie del arduino (USB OTG)
-	$scope.conectarSerie = function() {
-		if (typeof (window.serial) != "undefined") {
-			// Pedimos permiso para conectarnos al puerto serie
-			console.log('Solicitando permiso para USB...');
-			window.serial.requestPermission(function() {
-				// Hemos obtenido permiso
-				console.log('Permiso concedido');
-				var opc = {
-					baudRate : 9600,
-					sleepOnPause : true
-				// Mantenemos el puerto abierto cuando la app pasa a 2º plano
-				};
-
-				// Nos conectamos
-				console.log('Abriendo puerto serie...');
-				window.serial.open(opc, function() {
-					console.log('Puerto abierto');
-					$scope.todoConectado();
-				}, $scope.errorSerie);
-			}, $scope.errorSerie);
-		} else
-			$scope.errorSerie("Dispositivo serie no disponible");
-
-	};
-
-	$scope.errorSerie = function(err) {
-		console.log(err);
-		$scope.desconectarTodo();
-		$ionicPopup.alert({
-			title : 'Error de conexión USB',
-			template : err
-		});
-	};
-
-	$scope.todoConectado = function() {
-		$scope.conectando = false;
-		$scope.estado.servicio = true;
-	};
-
-	$scope.desconectarTodo = function() {
-		if ($scope.estado.mqtt || $scope.conectandoMQTT) {
-			console.log('Desconectamos MQTT');
-			if (MqttClient.connected)
-				MqttClient.disconnect();
-		}
-
-		if ($scope.estado.arduinoCon) {
-			console.log('Cerramos puerto serie');
-			window.serial.close(function() {
-				console.log('Puerto cerrado');
-			}, function() {
-			});
-		}
-
-		$scope.estado.mqtt = false;
-		$scope.estado.arduinoCon = false;
-		$scope.estado.arduinoIni = false;
-		$scope.conectando = false;
-		$scope.conectandoMQTT = false;
-		$scope.estado.servicio = false;
-	};
-
-	$scope.iniciarDetener = function() {
-		if ($scope.conectando || $scope.estado.servicio) {
-			$scope.desconectarTodo();
-		} else {
-			$scope.conectando = true;
-			$scope.conectarMQTT($rootScope.mqtt.servidor,
-					$rootScope.mqtt.puerto, $rootScope.mqtt.usuario,
-					$rootScope.mqtt.password);
-		}
-	};
-});
